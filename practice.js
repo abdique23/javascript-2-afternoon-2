b01dd69b2cae35db53c80737e3514e40e30527eb
abdique23 <abdique23@gmail.com>
2020-01-08 16:28:17 -0600

    2nd afternoon project

--- practice.js
+++ practice.js
@@ -17,7 +17,9 @@ var arr = [10,20,30];
   Return the first item in the given array.
 */
 
-//Code Here
+function first(arr) {
+  return arr[0]
+};
 
 
 
@@ -32,7 +34,9 @@ var arr = [40,50,60];
   Return the last item in the given array.
 */
 
-//Code Here
+function last(arr) {
+  return arr[2]
+};
 
 
 
@@ -49,6 +53,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 
 //Code Here
 
+function looper(family) {
+   
+  for (let i=0; i <= family.length -1; i++)
+  alert(family[i])
+};
 
 
 ////////// PROBLEM 4 //////////
@@ -63,6 +72,12 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
+
+function reversedLooper(letters) {
+  for (let i=letters.length -1; i>= 0; i--)
+  alert(letters[i])
+};
+ 
 
 
 
@@ -79,8 +94,20 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 
 //Code Here
 
+function evenFinder(nums) {
+  let evenNumbers = [];
+  for (i = 0; i<= nums.length; i++){
+    if (nums[i] %2 === 0)
+  evenNumbers.push(nums[i]);
+}
+return evenNumbers
+  }
+
+  
 
 
+
+evenFinder()
 
 
 
